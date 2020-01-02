54a9d4705625bf970d0cad5f81e9c3ddac33c850
Dennys Fredericci <dennys.fredericci@kbc.be>
2020-01-02 08:12:35 +0100

    Add greetings

--- src/main/java/br/com/fredericci/devoctans/HomeController.java
+++ src/main/java/br/com/fredericci/devoctans/HomeController.java
@@ -2,13 +2,28 @@ package br.com.fredericci.devoctans;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
-        return "home/index";
+    public ModelAndView index(@RequestParam(required = false, name = "name") String name) {
+
+
+        ModelAndView view = new ModelAndView("home/index");
+
+
+        String welcomeMessage = Optional.ofNullable(name)
+                .map("Welcome "::concat)
+                .orElse("");
+
+        view.addObject("greetings", welcomeMessage);
+
+        return view;
     }
 
 }
